9822cc73eea13a4ae4e9edc5aeb7595fc9a106a1
nikunj <nikunjgoyal31@gmail.com>
2020-08-25 15:54:25 +0530

    readyingbasicstructurecommit 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home";
+import MyLearningPath from "./pages/MyLearningPath";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+
+        <Route exact path="/MyLearningPath" component={MyLearningPath} />
+
+        <Route exact path="/Projects" component={Projects} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/MyLearningPath.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyLearningPath() {
+  return <div></div>;
+}
+
+export default MyLearningPath;
